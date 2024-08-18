690cee14c04f2d44bfc97950e5fc671c4384bae0
Mio-K0408 <mio.love.k@gmail.com>
2024-08-18 13:33:18 +0900

    ABC367 復習

--- series_abc/ABC367/B - Cut.py
+++ series_abc/ABC367/B - Cut.py
@@ -1,23 +1,52 @@
-X = input()
-ans = ""
-flg = False
+"""
+    答案1（リアタイ）
+"""
 
-for item in reversed(X):
-    if flg:
-        ans += item
-    else:
-        if item == ".":
-            ans += item
-            flg = False
-        elif item == "0" and not ans:
-            pass
-        else:
-            ans+=item
-if ans[0] == ".":
-    ans = ans[1:]
-    reverse_list = list(reversed(ans))
-else:
-    reverse_list = list(reversed(ans))
+# X = input()
+# ans = ""
+# flg = False
 
-print("".join(reverse_list))
+# for item in reversed(X):
+#     if flg:
+#         ans += item
+#     else:
+#         if item == ".":
+#             ans += item
+#             flg = False
+#         elif item == "0" and not ans:
+#             pass
+#         else:
+#             ans+=item
+# if ans[0] == ".":
+#     ans = ans[1:]
+#     reverse_list = list(reversed(ans))
+# else:
+#     reverse_list = list(reversed(ans))
+
+# print("".join(reverse_list))
+
+# ------------------------------------------
+"""
+    答案2
+    言語の機能である、型変換を使う
+    ※ 言語仕様によるので他の解法も知っておくほうがいい。
+"""
+# x = float(input())
+# # is_integer():整数か判定（小数点以下が0）
+# # int():int型に変換、
+# if (x.is_integer()):
+#   print(int(x))
+# else:
+#   print(x)
     
+# ------------------------------------------
+"""
+    答案3
+    文字列として扱い、不要な文字を削除していく。
+    言語仕様によらないので、どの言語でも使える。
+"""
+X = input()
+
+while X[-1] == "0":X=X.rstrip("0")
+if X[-1] == ".":X=X.rstrip(".")
+print(X)
--- series_abc/ABC367/C - Enumerate Sequences.py
+++ series_abc/ABC367/C - Enumerate Sequences.py
@@ -1,30 +1,47 @@
+"""
+    答案1（リアタイ）
+    ※ 未提出
+"""
+# N,K = map(int,input().split())
+# R = list(map(int,input().split()))
+# for i in range(N-1,-1,-1):
+#     list1 = [1]*N
+#     if i == N-1:
+#         pass
+#     elif R[i] != 1:
+#         list1[i] = 2
+#     elif R[i] == 1:
+#         continue    
+#     for j in range(N-1,i-1,-1):  
+#         print("{} {}".format(i,j))
+#         if sum(list1) % K == 0:
+#             print(" ".join(map(str,list1)))
+#             tmp = list1[j]
+#             list1[j] = tmp+1
+#             print("{} {} {} {}".format("上",list1[j],R[j],list1))   
+#             if list1[j]>R[j]:
+#                 continue
+#         else:
+#             tmp = list1[j]
+#             list1[j] = tmp+1                
+#             print("{} {} {} {}".format("中",list1[j],R[j],list1)) 
+#             if list1[j]>R[j]:              
+#                 continue
+
+# ------------------------------------------
+"""
+    答案2
+    再帰法（繰り返しよりメモリ使用量が多く、速度も遅い）
+"""
 N,K = map(int,input().split())
 R = list(map(int,input().split()))
-for i in range(N-1,-1,-1):
-    list1 = [1]*N
-    if i == N-1:
-        pass
-    elif R[i] != 1:
-        list1[i] = 2
-    elif R[i] == 1:
-        continue    
-    for j in range(N-1,i-1,-1):  
-        print("{} {}".format(i,j))
-        # if flg:
-        if sum(list1) % K == 0:
-            print(" ".join(map(str,list1)))
-            tmp = list1[j]
-            list1[j] = tmp+1
-            print("{} {} {} {}".format("上",list1[j],R[j],list1))   
-            if list1[j]>R[j]:
-                continue
-        else:
-            tmp = list1[j]
-            list1[j] = tmp+1                
-            print("{} {} {} {}".format("中",list1[j],R[j],list1)) 
-            if list1[j]>R[j]:              
-                continue
-        # else:
-        #     print("抜ける")
-        #     break
 
+def f(n, arr, s):
+    if n == N:
+        if s % K == 0:
+            print(' '.join(map(str, arr)))
+        return
+    for i in range(1, R[n]+1):
+        f(n+1, arr+[i], s+i)
+
+f(0, [], 0)
